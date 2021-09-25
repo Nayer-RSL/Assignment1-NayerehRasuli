0077d609f0b3c745bfd768b3dfe48be4ca7372a3
Nayereh Rasuli <Nrasuli1993@yahoo.com>
2021-09-24 23:45:28 -0400

    Add project files.

---
+++ WebApplication6/Controllers/HostingCostController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication6.Controllers
+{
+    public class HostingCostController : ApiController
+    {
+
+        public string Get(int id)
+        {
+
+            double HST = 0.13;
+            int FN = (id / 14) + 1;
+            double PriceFN = 5.5;
+            double Net = FN * PriceFN;
+            double Tax = HST * Net;
+            double Final = Tax + Net;
+            string Sen1 = FN + " fortnights at $5.50/FN = $" + Net + " CAD";
+            string Sen2 = "\n HST 13% = $" + Tax + " CAD";
+            string Sen3 = "\n Total = $" + Final + " CAD";
+            return Sen1 + Sen2 + Sen3;
+        }
+    }
+}
